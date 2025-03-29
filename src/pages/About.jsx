f575941beb3ac2a90384498be413fe03acf89272
Presly K.MALONGO <preslykomba.m@gmail.com>
2025-03-29 20:16:54 +0100

    Add commentary ImageBanner

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,7 +6,7 @@ import "./About.scss";
 function About() {                                                                                // On crée un composant "About" qui concerne la page dédiée aux informations ("A propos") sur l'entreprise.
     return (
         <>
-            <ImageBanner />
+            <ImageBanner />                                                                       {/* Le composant ImageBanner est utilisé pour afficher une image en haut de la page "A propos". Il est importé depuis le dossier components. */} 
             <div className="about__container">
                 <DescriptionPanel title="Fiabilité" content="C'est très important" />             {/* Selon la maquettte, la partie "A propos" contient 4 panels. Chaque panel contient un titre et un contenu. */}
                 <DescriptionPanel title="Respect" content="C'est très important" />
